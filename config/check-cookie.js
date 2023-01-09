9e8dc8d4bcf822483d0eb13d6584444b5185d606
chadiii <blackbeard@DESKTOP-E1T0NV5.localdomain>
2023-01-09 17:07:07 +0100

    adapt the change to workflow

--- config/check-cookie.js
+++ config/check-cookie.js
@@ -1,30 +1,6 @@
-function checkCookie(r) {
-    let requestMethod;
-    let cookies;
-    if (r.headersIn['Cookie']) {
-        cookies = r.headersIn['Cookie']
-        requestMethod = "GET"
-        r.headersOut["X-cookie-header"] = cookies;
-        r.log(1)
-        //r.headersOut["x-Cookie-ex"] = cookies.fs_experiences;
-    } else {
-        requestMethod = "HEAD"
-        //r.headersOut["Set-Cookie"] = `fs_experiences=${r.headersIn['x-fs-visitor']}@${r.headersIn['x-fs-visitor']}; path=/; domain=localhost`;
-        r.log(2)
-    }
-
-    r.headersOut["X-method"] = requestMethod
-    r.log(3)
-    return requestMethod
-}
-
-function addCookie(r) {
-    r.headersOut["Set-Cookie"] = `fs_experiences=${r.headersOut['x-fs-visitor']}@${r.headersOut['x-fs-experiences']}; path=/; domain=localhost`;
-}
-
-async function callDecisionApi(r) {
+async function GetHeader(r) {
     if (!r.headersIn['Cookie']) {
-        let reply = await ngx.fetch('http://localhost:8081', {
+        let reply = await ngx.fetch('http://node-app:8081/server', {
             method: "HEAD",
             headers: {
                 'Content-Type': 'appliation/json',
@@ -33,8 +9,12 @@ async function callDecisionApi(r) {
         }
         );
         let text = await reply.headers;
-        r.headersOut["X-header-decision-api"] = text;
+        r.headersOut["x-fs-experiences"] = text.get('x-fs-visitor') + "@" + text.get('x-fs-experiences');
+        r.headersOut["x-fs-combination"] = text.get('x-fs-experiences');
+        r.headersOut["x-fs-visitor"] = text.get('x-fs-visitor');
+        r.headersOut["x-set-cookie"] = "fs_experiences=" + text.get('x-fs-visitor') + "@" + text.get('x-fs-experiences') + "; path=/; domain=localhost";
     }
-    r.return(200)
+    r.return(200);
 }
-export default { checkCookie, addCookie, callDecisionApi }
+
+export default { GetHeader }
--- src/index.js
+++ src/index.js
@@ -54,49 +54,7 @@ const homeHandler = async (req, res) => {
 
 }
 
-app.get('/', homeHandler)
-
-app.get('/get_cookie', async (req, res) => {
-
-  const flagship = new Flagship(process.env.FS_ENV_ID, process.env.FS_API_KEY);
-
-  let visitorId
-
-  res.setHeader('Cache-Control', 'max-age=1, s-maxage=600');
-  res.setHeader("Content-Type", "text/html");
-
-  if (!req.get('x-fs-visitor')) {
-    visitorId = flagship.generateUID();
-  } else {
-    visitorId = req.get('x-fs-visitor');
-  }
-
-  await flagship.start(
-    visitorId,
-    {
-      nbBooking: 4,
-    },
-  );
-
-  let cacheKey = flagship.getHashKey();
-
-  let FsFlag = flagship.getFlag('restaurant_cta_review_text', 'Leave a Review')
-
-  if (cacheKey === false) {
-    cacheKey = 'optout';
-    visitorId = 'ignore-me';
-  }
-
-  //let experiencesCookie = cacheKey === 'optout' ? cacheKey : `${visitorId}@${cacheKey}`;
-  res.setHeader('x-fs-visitor', visitorId);
-  res.setHeader('x-fs-flags', FsFlag);
-  res.setHeader('x-fs-experiences', cacheKey);
-
-  res.cookie('fs_experiences', `${visitorId}@${cacheKey}`, { domain: 'localhost' })
-
-  res.end()
-
-})
+app.get('/server', homeHandler)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
