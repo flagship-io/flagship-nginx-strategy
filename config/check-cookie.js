41b5facd5f472ce9c6c2ecc51119409d44dfde4f
chadiii <blackbeard@DESKTOP-E1T0NV5.localdomain>
2023-01-11 11:01:48 +0100

    hash the campaign_id/variation_id combination

--- config/check-cookie.js
+++ config/check-cookie.js
@@ -1,5 +1,7 @@
+var crypto = require('crypto')
+
 async function GetHeader(r) {
-    if (!r.headersIn['Cookie']) {
+    if (!r.headersIn['Cookie'] || !r.headersIn['Cookie'].includes("fs_experiences")) {
         let reply = await ngx.fetch('http://node-app:8081/server', {
             method: "HEAD",
             headers: {
@@ -9,8 +11,11 @@ async function GetHeader(r) {
         }
         );
         let text = await reply.headers;
+        let hashExperiences = crypto.createHash('sha1').update(text.get('x-fs-experiences')).digest('base64url')
+
         r.headersOut["x-fs-experiences"] = text.get('x-fs-visitor') + "@" + text.get('x-fs-experiences');
         r.headersOut["x-fs-combination"] = text.get('x-fs-experiences');
+        r.headersOut["x-fs-combination-hash"] = hashExperiences;
         r.headersOut["x-fs-visitor"] = text.get('x-fs-visitor');
         r.headersOut["x-set-cookie"] = "fs_experiences=" + text.get('x-fs-visitor') + "@" + text.get('x-fs-experiences') + "; path=/; domain=localhost";
     }
